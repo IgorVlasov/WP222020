ce79082363a95864ef8dbf9b24728fee41f681b5
AndUha227 <andruha2271337@yandex.ru>
2021-01-31 11:24:41 +0000

    задача 1 бета

---
+++ Ukarainski.py
@@ -0,0 +1,15 @@
+def ainta():
+  a = int(input("Введите значение a: ")
+def bintb():
+  b = int(input("Введите значение b: ")  
+while True:
+  ainta()
+  if a = int(a):
+    bintb()
+    if b = int(b):
+    except:
+      print("Введено неверное значение")
+      bintb
+  except:
+    print("Введено неверное значение")
+    ainta()
