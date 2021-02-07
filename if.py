a70b6b5555ed105e71fa53ef501e0df40284185e
general in <itstepkuzminov@gmail.com>
2021-02-07 11:26:23 +0000

    While

--- if.py
+++ if.py
@@ -1,6 +1,8 @@
-X = int(input())
-if 5 < X < 10:
+x = int(input())
+if 5 < x < 10:
   print("mqwkjmwgogim")
+elif x==0:
+  print("0")
 else :
   print("fdafa")
   print("ничего не понимаю,зачем ты мне шлёшь какие-то цыфры")
---
+++ while.py
@@ -0,0 +1,27 @@
+x = 1
+while true:
+  x = x * 11
+  for i in range(x*10):
+    print(x ," ")
+    x = x * 11
+    for i in range(x*10):
+      print(x ," ")
+      x = x * 11
+      for i in range(x*10):
+        print(x ," ")
+        x = x * 11
+        for i in range(x*10):
+          print(x ," ")
+          x = x * 11
+          for i in range(x*10):
+            print(x ," ")
+            x = x * 11
+            for i in range(x*10):
+              print(x ," ")
+              x = x * 11
+              for i in range(x*10):
+                print(x ," ")
+                x = x * 11
+                for i in range(x*10):
+                  print(x ," ")
+                  x = x * 11
