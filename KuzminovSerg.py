3bac3b7af21a1097adc3f47c0e2b1778fc535774
general in <itstepkuzminov@gmail.com>
2021-02-07 10:59:06 +0000

    If If not If Else If is If

--- KuzminovSerg.py
+++ KuzminovSerg.py
@@ -1,8 +1,5 @@
-a = 0; # зачем?
-b = 0; # зачем?
-print("a")
-a = int(input())
-print("b")
-b = int(input())
-c = a**(2+b) - b**(2+a)
-print(c)
+X = int(input())
+if 5 < X < 10:
+  print("mqwkjmwgogim")
+else :
+  print("fdafa")
