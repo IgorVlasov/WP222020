0460a9a4b488bb3731116a14a3207a048a2fb465
Sashagif <makaryev.sasha@gmail.com>
2021-02-14 11:50:15 +0000

    ,

--- todo.py
+++ todo.py
@@ -1,30 +1,36 @@
-HELP = """
-help - список команд
-add - добавить событие
-showall - показать все события
-remove - удалить событие
-exit - закрыть программу
+HELP ="""
+help       - Помощ душевно больным
+add        - Добавить 
+showall    - Показать события
+remove     - Удалить событие 
+exit       - Закрыть программу
 """
 exit = False
 userUnswer = 0
-
 todo = {}
 print("Введите команду")
 print("Для вывода списка команд введите help")
-
-while exit == False:
-  userUnswere = input()
-
-  if userUnswere == "exit":
-    exit = True
+while exit == False :
+  userUnswer = input()
+  if userUnswer == "exit":
+    exit = True 
     continue
-  elif userUnswere == "add":
-    print("Событие добавлено")
-  elif userUnswere == "rewove":
-    print("Событие удалено")
-  elif userUnswere == "showall":
-    print("События добавлены")
-  elif userUnswere == "help":
-    print(HELP)
-  else:
-    print("Ты чё ввёл гений???")
+  elif userUnswer == "add":
+    print("durak")
+    addKey = input()
+    print("nu nu")
+    addValue = input()
+    if addKey in todo.keys:
+      tmp = list(todo[addKey])
+      tmp += addValue
+    else:
+      
+    todo[addKey] = addValue
+  elif userUnswer == "showall":
+    print(todo)
+  elif userUnswer == "remove":
+    print("тупой")
+  elif userUnswer == "help":
+    print("HELP!!!" + HELP)
+  else :
+    print("Даун?")
