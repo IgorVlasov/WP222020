a6cddb484ac07db937ec503f71c9aaf03f6d0c88
Igor Vlasov <vlasov.work@gmail.com>
2021-02-21 11:53:53 +0000

    def aDD def checkDate

--- todo.py
+++ todo.py
@@ -1,42 +1,66 @@
+import time
+
 HELP = """
-help    - список команд
-add     - добавить событие
-showall - показать все события
-remove  - удалить событие
-exit    - закрыть программу
+help      - список команд
+add       - добавить задачу
+showall   - показать все задачи
+show      - задачи на конкретную дату
+remove    - удалить задачу
+exit      - закрыть программу
 """
-exit = False
+
+def checkDate(uDate):
+  try:
+    time.strptime(uDate, '%d.%m.%Y')
+    return True
+  except ValueError:
+    print('Не правильный формат даты')
+    return False
+
+def addTask(uDate, uTask):
+  if uDate in todo.keys():
+    todo[uDate].append(uTask)
+  else:
+    todo[uDate] = [uTask]
+  print( f"На {uDate} добвалена задача {uTask}" )
+  
+
 userUnswer = 0
 
 todo = {}
 print("Введите команду")
 print("Для вывода списка команд введите help")
 
-while exit == False:
+while True:
   
   userUnswer = input()
 
   if userUnswer == "exit":
-    exit = True
-    continue
+    print("Досвидания!")
+    break
   elif userUnswer == "add":
-    print("Введите дату события")
-    addKey = input()
-    print("Что нужно сделать?")
-    addValue = input()
+    uDate = input("Введите дату события в формате дд.мм.гггг\n")
+    
+    if checkDate(uDate) == False:
+      continue
+    
+    uTask = input("Что нужно сделать?\n")
+    addTask(uDate, uTask)
 
-    if addKey in todo.keys():
-      tmp = [todo[addKey]]
-      tmp.append(addValue)
-      todo[addKey] = tmp
-    else:
-      todo[addKey] = addValue
-
-    print("событие добавлено")
   elif userUnswer == "remove":
     print("событие удалено")
   elif userUnswer == "showall":
-    print(todo)
+    for pDate in sorted( todo.keys() ):
+      for pTask in todo[pDate]:
+        print(f"[{pDate}]\t{pTask}")
+    print()
+  elif userUnswer == "show":
+    uDate = input("Ведите дату\n")
+    if checkDate(uDate) == False:
+      continue 
+    for pTask in todo[uDate]:
+        print(f"[{uDate}]\t{pTask}")
+
   elif userUnswer == "help":
     print(HELP)
   else:
